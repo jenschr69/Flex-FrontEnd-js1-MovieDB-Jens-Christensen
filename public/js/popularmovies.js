cf3574efbcd9eca09c82618cff7dcaa6e2845489
Jens Christensen - Glodexa <jens@glodexa.com>
2025-10-19 19:34:11 +0200

    Updating api url for top movies

--- public/js/popularmovies.js
+++ public/js/popularmovies.js
@@ -4,7 +4,7 @@
 
 let BAERER_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGE3NWZjZjU1NDMxNTI4NzBjNzliZTRkNzk1M2EzOSIsIm5iZiI6MTc2MDA4MjIwOC40NCwic3ViIjoiNjhlOGI5MjBhNGQ0ZWFlNWU5NGE5YjQ0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.bVbZEk72a6panZkcwNMFNRNIQZh-b0nFIQMu6mcCHaI";
 const apiUrl= "https://api.themoviedb.org/3/movie/popular";
-const moviesContainer = document.getElementById("movies");
+const moviesContainer = document.getElementById("displaySearchResults");
 
 const myHeaders = new Headers();
 myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGE3NWZjZjU1NDMxNTI4NzBjNzliZTRkNzk1M2EzOSIsIm5iZiI6MTc2MDA4MjIwOC40NCwic3ViIjoiNjhlOGI5MjBhNGQ0ZWFlNWU5NGE5YjQ0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.bVbZEk72a6panZkcwNMFNRNIQZh-b0nFIQMu6mcCHaI");
@@ -31,17 +31,19 @@ async function loadmovie(params) {
   
 } 
 
-
 function createMovieCard (media) {
     const { title, backdrop_path, release_date } = media;
 
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie_item")
+    movieCard.classList.add("border-2")
+    movieCard.classList.add("rounded-lg")
+    movieCard.classList.add("m-2")
 
     movieCard.innerHTML = `
-    <img src="https://image.tmdb.org/t/p/w500${backdrop_path}" class="movie_image_rounded">
-    <div class="title"> ${title} </div>
-    <div class="title"> ${release_date} </div>    
+    <img src="https://image.tmdb.org/t/p/w300${backdrop_path}">
+    <div class="pl-2 text-lg"> ${title} </div>
+    <div class="pl-2 text-sm">Release date: ${release_date} </div>    
    `;
    return movieCard;
 }
--- public/js/topratedmovies.js
+++ public/js/topratedmovies.js
@@ -4,7 +4,7 @@
 
 let BAERER_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMGE3NWZjZjU1NDMxNTI4NzBjNzliZTRkNzk1M2EzOSIsIm5iZiI6MTc2MDA4MjIwOC40NCwic3ViIjoiNjhlOGI5MjBhNGQ0ZWFlNWU5NGE5YjQ0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.bVbZEk72a6panZkcwNMFNRNIQZh-b0nFIQMu6mcCHaI";
 let apiKey = "00a75fcf5543152870c79be4d7953a39";
-const apiUrl= "https://api.themoviedb.org/3/movie/top_rated?results=10";
+const apiUrl= "https://api.themoviedb.org/3/movie/top_rated";
 const moviesContainer = document.getElementById("displaySearchResults");
 
 const myHeaders = new Headers();
